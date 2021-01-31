3bd54647d5f05af94adaee560d02578e6c35fdd7
MebeaN <se.mebea.nibret@gmail.com>
2021-01-31 19:08:29 +0300

    5TH Commit

--- Calculator/assets/js/app.js
+++ Calculator/assets/js/app.js
@@ -1,131 +1,253 @@
-// The functions to be performed
-function add(a,b){
-    return "The result is " + Number(a) + Number(b);
+var ex = document.getElementById("result");
+var final =document.getElementById("final");
+
+function plus(){
+    
+    var ext = ex.innerText
+    var len = ext.length
+    if(ext){
+        if((ext.charAt(len-1) == "+") || (ext.charAt(len-1) == "-") || (ext.charAt(len-1) == "/") || (ext.charAt(len-1) == "*")){
+            return
+        }
+        ex.innerText = ext + "+"
+    }
+    
 }
 
-function sub(a,b){
-    return "The result is " +  Number(a) - Number(b);
+function minus(){
+    var ext = ex.innerText
+    var len = ext.length
+    if(ext){
+        if((ext.charAt(len-1) == "+") || (ext.charAt(len-1) == "-") || (ext.charAt(len-1) == "/") || (ext.charAt(len-1) == "*")){
+            return
+        }
+        ex.innerText = ext + "-"
+    }
 }
 
-function mul(a,b){
-    return "The result is " +  Number(a) * Number(b);
+function times(){
+    var ext = ex.innerText
+    var len = ext.length
+
+    if(ext){
+        if((ext.charAt(len-1) == "+") || (ext.charAt(len-1) == "-") || (ext.charAt(len-1) == "/") || (ext.charAt(len-1) == "*")){
+            return
+        }
+        ex.innerText = ext + "*"
+    }
 }
 
-function div(a,b){
-    if(b != 0){return "The result is " +  (Number(a) / Number(b))}
-    return "change the second number."
+function over(){
+    var ext = ex.innerText
+    var len = ext.length
+    if(ext){
+        if((ext.charAt(len-1) == "+") || (ext.charAt(len-1) == "-") || (ext.charAt(len-1) == "/") || (ext.charAt(len-1) == "*")){
+            return
+        }
+        ex.innerText = ext + "/"
+    }
 }
 
-function max(a,b){
-    if(Number(a) > Number(b)){return "The result is " +  a}
-    else{return "The result is " +  b}
+
+function equals(){
+    var x = eval(ex.innerText)
+    
+    if(x){
+        final.innerText = x;
+        ex.innerText = ""
+    }
+      
+    if(final.innerText == Infinity){
+        final.innerText = "Invalid "
+    }
 }
 
-function min(a,b){
-    if(Number(a) < Number(b)){return "The result is " +  a}
-    else{return "The result is " +  b}
+function clear(){
+    ex.innerText = ""
+    final.innerText = ""
+
 }
 
-function average(a,b){
-    return "The result is " + (Number(a)+Number(b))/2;
+function square(){
+    var x = ex.innerText
+    if(x) { 
+        final.innerText = (eval(x))**2
+        ex.innerText = ""
+    }
 }
 
-function square(a){
-    return "The result is " +  a*a;
+function sqroot(){
+    var x = ex.innerText
+    if(x) { 
+        final.innerText = (eval(x))**(1/2)
+        ex.innerText = ""
+    }
 }
 
-function addMany(){
-    var value = prompt("How many numbers do you wanna add?");
+const equal = document.getElementById("equal")
+equal.addEventListener("click", equals)
 
-    //Take the numbers to be calculated step by step
-    numbers = new Array();
-    for(let i = 0; i < parseInt(value); i++){
-        numbers[i] = prompt("Enter number");   
-    }
+const plusSign = document.getElementById("plus")
+plusSign.addEventListener("click", plus)
+const minusSign = document.getElementById("minus")
+minusSign.addEventListener("click", minus)
+const timesSign = document.getElementById("times")
+timesSign.addEventListener("click", times)
+const overSign = document.getElementById("over")
+overSign.addEventListener("click", over)
+const sq = document.getElementById("square")
+const sqrt = document.getElementById("sqRoot")
+sq.addEventListener("click",square)
+sqrt.addEventListener("click",sqroot)
 
-    // Do the multiplication of the numbers
-    var a = 0;
-    for(let i = 0; i < parseInt(value); i++){
-        a = Number(a) + Number(numbers[i]);
-    }
+const cl = document.getElementById("clear")
+cl.addEventListener("click", clear)
 
-    return "The result is " + a;
-}
 
-function mulMany(){
-    var value = prompt("How many numbers do you wanna multiply?");
 
-    //Take the numbers to be calculated step by step
-    numbers = new Array();
-    for(let i = 0; i < parseInt(value); i++){
-        numbers[i] = prompt("Enter number");   
-    }
 
-    // Do the multiplication of the numbers
-    var a = 1;
-    for(let i = 0; i < parseInt(value); i++){
-        a = a * parseInt(numbers[i]);
-    }
 
-    return "The result is " + a;
-}
 
-// The IIFEs statement
-(function() {
-    let service = prompt("Which service do you want?  1, Addition  2, Subtraction  3, Multiplication  4, Division 5, Maximum 6, Minimum 7, average 8, square 9, Addition of many numbers 10, Multiplication of many numbers")
 
-    if(service == 1){
-        var a = prompt("Enter first number")
-        var b = prompt("Enter second number")
-        console.log(add(a,b))
-    }
 
-    if(service == 2){
-        var a = prompt("Enter first number")
-        var b = prompt("Enter second number")
-        console.log(sub(a,b))
-    }
 
-    if(service == 3){
-        var a = prompt("Enter first number")
-        var b = prompt("Enter second number")
-        console.log(mul(a,b))
-    }
 
-    if(service == 4){
-        var a = prompt("Enter first number")
-        var b = prompt("Enter second number")
-        console.log(div(a,b))
-    }
 
-    if(service == 5){
-        var a = prompt("Enter first number")
-        var b = prompt("Enter second number")
-        console.log(max(a,b))
-    }
 
-    if(service == 6){
-        var a = prompt("Enter first number")
-        var b = prompt("Enter second number")
-        console.log(min(a,b))
-    }
 
-    if(service == 7){
-        var a = prompt("Enter first number")
-        var b = prompt("Enter second number")
-        console.log(average(a,b))
-    }
 
-    if(service == 8){
-        var a = prompt("Enter a number")
-        console.log(square(a))
-    }
 
-    if(service == 9){
-        console.log(addMany())
-    }
 
-    if(service == 10){
-        console.log(mulMany())
-    }
-})();
+
+
+// // The functions to be performed
+// function add(a,b){
+//     return "The result is " + Number(a) + Number(b);
+// }
+
+// function sub(a,b){
+//     return "The result is " +  Number(a) - Number(b);
+// }
+
+// function mul(a,b){
+//     return "The result is " +  Number(a) * Number(b);
+// }
+
+// function div(a,b){
+//     if(b != 0){return "The result is " +  (Number(a) / Number(b))}
+//     return "change the second number."
+// }
+
+// function max(a,b){
+//     if(Number(a) > Number(b)){return "The result is " +  a}
+//     else{return "The result is " +  b}
+// }
+
+// function min(a,b){
+//     if(Number(a) < Number(b)){return "The result is " +  a}
+//     else{return "The result is " +  b}
+// }
+
+// function average(a,b){
+//     return "The result is " + (Number(a)+Number(b))/2;
+// }
+
+// function square(a){
+//     return "The result is " +  a*a;
+// }
+
+// function addMany(){
+//     var value = prompt("How many numbers do you wanna add?");
+
+//     //Take the numbers to be calculated step by step
+//     numbers = new Array();
+//     for(let i = 0; i < parseInt(value); i++){
+//         numbers[i] = prompt("Enter number");   
+//     }
+
+//     // Do the multiplication of the numbers
+//     var a = 0;
+//     for(let i = 0; i < parseInt(value); i++){
+//         a = Number(a) + Number(numbers[i]);
+//     }
+
+//     return "The result is " + a;
+// }
+
+// function mulMany(){
+//     var value = prompt("How many numbers do you wanna multiply?");
+
+//     //Take the numbers to be calculated step by step
+//     numbers = new Array();
+//     for(let i = 0; i < parseInt(value); i++){
+//         numbers[i] = prompt("Enter number");   
+//     }
+
+//     // Do the multiplication of the numbers
+//     var a = 1;
+//     for(let i = 0; i < parseInt(value); i++){
+//         a = a * parseInt(numbers[i]);
+//     }
+
+//     return "The result is " + a;
+// }
+
+// // The IIFEs statement
+// (function() {
+//     let service = prompt("Which service do you want?  1, Addition  2, Subtraction  3, Multiplication  4, Division 5, Maximum 6, Minimum 7, average 8, square 9, Addition of many numbers 10, Multiplication of many numbers")
+
+//     if(service == 1){
+//         var a = prompt("Enter first number")
+//         var b = prompt("Enter second number")
+//         console.log(add(a,b))
+//     }
+
+//     if(service == 2){
+//         var a = prompt("Enter first number")
+//         var b = prompt("Enter second number")
+//         console.log(sub(a,b))
+//     }
+
+//     if(service == 3){
+//         var a = prompt("Enter first number")
+//         var b = prompt("Enter second number")
+//         console.log(mul(a,b))
+//     }
+
+//     if(service == 4){
+//         var a = prompt("Enter first number")
+//         var b = prompt("Enter second number")
+//         console.log(div(a,b))
+//     }
+
+//     if(service == 5){
+//         var a = prompt("Enter first number")
+//         var b = prompt("Enter second number")
+//         console.log(max(a,b))
+//     }
+
+//     if(service == 6){
+//         var a = prompt("Enter first number")
+//         var b = prompt("Enter second number")
+//         console.log(min(a,b))
+//     }
+
+//     if(service == 7){
+//         var a = prompt("Enter first number")
+//         var b = prompt("Enter second number")
+//         console.log(average(a,b))
+//     }
+
+//     if(service == 8){
+//         var a = prompt("Enter a number")
+//         console.log(square(a))
+//     }
+
+//     if(service == 9){
+//         console.log(addMany())
+//     }
+
+//     if(service == 10){
+//         console.log(mulMany())
+//     }
+// })();
